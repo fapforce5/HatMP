1cdefc81a6c1a3df44aac684947ed299cc12416f
Shteryan Nikolaev <actepukc@gmail.com>
2026-07-25 23:11:11 +0100

    Fix room589 event state lookup

--- scripts/room/room589.js
+++ scripts/room/room589.js
@@ -1,6 +1,7 @@
 ﻿//Liam
 var room589 = {};
 room589.main = function () {
+    let roomEvents = room585.getRoomsNow(false);
     switch (Math.floor(g.gethourdecimal())) {
         case 20:
         case 21:
